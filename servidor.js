aba65ec5bd50e4b4ebe86aae953cbee66d571694
Usuario <rflores9@ucol.mx>
2019-08-27 09:48:59 -0500

    servidor

---
+++ servidor.js
@@ -0,0 +1,22 @@
+const express =require ('exoress');
+const app = express ();
+
+app.get('/', (req, res) => {
+res.send({ hi: 'there'});
+
+});
+
+const PORT = process.env.PORT
+app.listen(PORT);
+
+// node --v
+// npm --v
+// git version 2.20.1.windows.1
+/*
+C:\Users\tecnologia\Documents\- Mis Documentos\Code\hypermedia>heroku create
+Creating app... done, ⬢ intense-sierra-29448
+https://intense-sierra-29448.herokuapp.com/ | https://git.heroku.com/intense-sierra-29448.git
+
+*/ 
+
+//heroku open = hola mundo
