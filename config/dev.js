72852d9d1bf87dde609a09e4b9b62268540bd15e
Rebe <rflores9@ucol.mx>
2019-09-24 08:30:11 -0500

    emaly-prod

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '384904718148-6og6o7lutvjksn09ksfs8h759p20o2dk.apps.googleusercontent.com',
+    googleClientSecret: 'mvcQu1c_q_caWXO99VYUwHqv',
+    moongoURL: 'mongodb+srv://rebe:rebe1234@cluster0-b0kvx.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'juaskjuaskjuaskjuaskjuaskjuaskjuaskjuaskjuaskjuaskkkkk'
+};
+
+//mongodb+srv://rebe2:s737@cluster0-0twzl.mongodb.net/test?retryWrites=true&w=majority
--- servidor.js
+++ servidor.js
@@ -1,17 +1,31 @@
-const express =require ('express');
-const passport = require ('passport');
-const GoogleStrategy = requiere('passport-google-oauth20').Strategy;
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./models/user');
+require('./services/passport');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+//Client ID 97191110036-lt6vr457dja7eipc1d3774cnb3391itj.apps.googleusercontent.com
+//CkientSecret 9lne-J7xkcI20dKFalKkMRMA
 
-app.get('/', (req, res) => {
-res.send({ hi: 'hola mundo'});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-});
+mongoose.connect(keys.moongoURL);
+authRoutes(app);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 
@@ -24,6 +38,6 @@ C:\Users\tecnologia\Documents\- Mis Documentos\Code\hypermedia>heroku create
 Creating app... done, ⬢ intense-sierra-29448
 https://intense-sierra-29448.herokuapp.com/ | https://git.heroku.com/intense-sierra-29448.git
 
-*/ 
+*/
 
 //heroku open = hola mundo
